48a080d506402b128849ec85b7dd22cf32b9a489
andersonhunter <andershu@oregonstate.edu>
2025-07-15 23:35:41 -0400

    flipped LED frame order, more natural

--- LEVELS/LEVEL_1/little_endian.gd
+++ LEVELS/LEVEL_1/little_endian.gd
@@ -6,11 +6,15 @@ var house_3 = "res://LEVELS/LEVEL_1/inside_house_3.tscn"
 var house_4 = "res://LEVELS/LEVEL_1/inside_house_4.tscn"
 
 func set_leds() -> void:
-	$LED_2.set_frame_and_progress(2, 0.0)
-	$LED_3.set_frame_and_progress(3, 0.0)
+	# Set the LED frames and begin playing
+	$LED_1.set_frame_and_progress(6, 0.0)
+	$LED_2.set_frame_and_progress(5, 0.0)
+	$LED_3.set_frame_and_progress(4, 0.0)
+	$LED_4.set_frame_and_progress(3, 0.0)
 	$LED_1.play()
 	$LED_2.play()
 	$LED_3.play()
+	$LED_4.play()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,7 +26,6 @@ func _ready() -> void:
 		$Player.start(Global.player_location)
 	else:
 		$Player.start($StartPosition.position)
-	# Add a delay or start from different frame
 	set_leds()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
