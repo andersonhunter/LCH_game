2f6092cf0cc1ee8e4a7eec88e7da24eaa7f13e4c
andersonhunter <andershu@oregonstate.edu>
2025-08-02 16:03:35 -0400

    added turn queue with sorting

--- LEVELS/LEVEL_2/battle_scene.gd
+++ LEVELS/LEVEL_2/battle_scene.gd
@@ -1,11 +1,15 @@
 extends Node2D
-var enemies = {
+var enemyStats = {
 	"slime": {
 		"health": 3,
 		"attack": 1,
-		"defense": 1
+		"defense": 1,
+		"speed": 1
 	}
 }
+
+var enemies: Array
+var turnQueue: Array
 
 var player = preload("res://LEVELS/LEVEL_2/player_battle.tscn").instantiate()
 
@@ -17,12 +21,28 @@ func addEnemies():
 			var enemy = load("res://LEVELS/LEVEL_2/slime.tscn").instantiate()
 			var parent := $enemies.get_children()[n]
 			parent.add_child(enemy)
+			enemies.push_back(enemy)
 	else:
 		for n in range(0, numEnemies):
 			var enemy = load("res://LEVELS/LEVEL_2/slime.tscn").instantiate()
 			var parent := $enemies.get_children()[n]
 			parent.add_child(enemy)
+			enemies.push_back(enemy)
+
+func sortTurnQueue():
+	# Sort the turn queue based on speed
+	enemies.sort_custom(
+		func(a, b): return enemyStats[a.name]["speed"] > enemyStats[b.name]["speed"]
+		)
+	turnQueue = enemies.duplicate()
+	for i in range(turnQueue.size()):
+		if enemyStats[turnQueue[i].name]["speed"] <= Global.playerStats["speed"]:
+			turnQueue.insert(i, player)
+			break
+		elif i == turnQueue.size() - 1:
+			turnQueue.push_back(player)
 
 func _ready() -> void:
 	get_node("player").add_child(player)
 	addEnemies()
+	sortTurnQueue()
