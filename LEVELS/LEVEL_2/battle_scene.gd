b1072b3edc2307441ef73e4f93c94f9a8daf72f4
andersonhunter <andershu@oregonstate.edu>
2025-08-01 20:26:52 -0400

    enemy spawn position depends on # enemies

--- LEVELS/LEVEL_2/battle_scene.gd
+++ LEVELS/LEVEL_2/battle_scene.gd
@@ -3,20 +3,25 @@ var enemies = {
 	"slime": {
 		"health": 3,
 		"attack": 1,
-		"defense": 1,
-		"res": "res://SPRITES/ENEMIES/slime_1.png"
+		"defense": 1
 	}
 }
 
 var player = preload("res://LEVELS/LEVEL_2/player_battle.tscn").instantiate()
 
 func addEnemies():
+	# Adds enemies to scene depending on how many enemies spawn [1..3]
 	var numEnemies = RandomNumberGenerator.new().randi_range(1, 3)
-	for n in range(0, numEnemies):
-		var enemy = load("res://LEVELS/LEVEL_2/slime.tscn").instantiate()
-		var parent := $enemies.get_children()[n]
-		# Wait until parent is ready, then add bat as sibling
-		parent.add_child(enemy)
+	if numEnemies == 2:
+		for n in range(1, 3):
+			var enemy = load("res://LEVELS/LEVEL_2/slime.tscn").instantiate()
+			var parent := $enemies.get_children()[n]
+			parent.add_child(enemy)
+	else:
+		for n in range(0, numEnemies):
+			var enemy = load("res://LEVELS/LEVEL_2/slime.tscn").instantiate()
+			var parent := $enemies.get_children()[n]
+			parent.add_child(enemy)
 
 func _ready() -> void:
 	get_node("player").add_child(player)
