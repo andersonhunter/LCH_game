3dffef45fec14b208241bcd8efa39e5f7759d60d
andersonhunter <andershu@oregonstate.edu>
2025-08-06 23:59:50 -0400

    added tilemap and player

--- LEVELS/LEVEL_2/silicon_swamp.gd
+++ LEVELS/LEVEL_2/silicon_swamp.gd
@@ -3,7 +3,7 @@ var battleScene = preload("res://LEVELS/LEVEL_2/battle_scene.tscn").instantiate(
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.add_child(battleScene)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
