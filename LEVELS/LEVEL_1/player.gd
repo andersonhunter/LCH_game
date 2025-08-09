fcac470a3cd019e556e2b1a1a0ba4b81e30dc1f9
andersonhunter <andershu@oregonstate.edu>
2025-08-08 21:05:15 -0400

    added collision handler for mold mite

--- LEVELS/LEVEL_1/player.gd
+++ LEVELS/LEVEL_1/player.gd
@@ -3,7 +3,9 @@ extends CharacterBody2D
 var speed = Global.speed
 var screen_size
 var bat = preload("res://LEVELS/LEVEL_1/bat.tscn").instantiate()
+var darkOverworld = preload("res://LEVELS/LEVEL_1/dark_overworld.tscn").instantiate()
 signal mite_1_collide
+@onready var rayCast = $RayCast2D
 
 func start(pos):
 	position = pos
@@ -16,11 +18,21 @@ func _ready():
 	Global.position_array.resize(100)
 	Global.position_array.fill(position)
 	# If character has bat companion, add them
-	if Global.has_bat:
+	if Global.has_bat and Global.isDark:
 		var parent := get_parent()
 		# Wait until parent is ready, then add bat as sibling
 		parent.ready.connect(func():
-			add_sibling(bat)
+			parent.add_child(bat)
+			parent.add_child(darkOverworld)
+		)
+		bat.hide()
+		bat.position = position
+	elif Global.has_bat:
+		var parent := get_parent()
+		# Wait until parent is ready, then add bat as sibling
+		parent.ready.connect(func():
+			parent.add_child(bat)
+			parent.add_child(darkOverworld)
 		)
 		bat.hide()
 		bat.position = position
@@ -33,15 +45,19 @@ func _process(delta: float) -> void:
 	# Perform raycasting logic
 	if input != Vector2.ZERO:
 		# Moving, so rotate raycast
-		$RayCast2D.rotation_degrees = rad_to_deg(input.angle()) + 90.
-	if $RayCast2D.is_colliding():
+		rayCast.rotation_degrees = rad_to_deg(input.angle()) + 90.
+	if rayCast.is_colliding():
 		# Collision detection for ray
-		var collider = $RayCast2D.get_collider()
+		var collider = rayCast.get_collider()
 		if collider.name == "mold_mite_1":
 			# Maybe add a switch statement function for collisions?
 			# match (collider.name) case "mold_mite_1": startConversation
 			# Also need rotation of raycast
 			mite_1_collide.emit()
+	if get_last_slide_collision():
+		match get_last_slide_collision().get_collider().name:
+			"mold_mite_1":
+				mite_1_collide.emit()
 	velocity = input * speed
 	move_and_slide()
 	if get_real_velocity() != Vector2.ZERO:
