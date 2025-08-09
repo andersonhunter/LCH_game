36c1522685d44001cd0831e578faa542b7a52787
andersonhunter <andershu@oregonstate.edu>
2025-08-08 21:04:54 -0400

    cleaned up

--- LEVELS/LEVEL_1/mite_1.gd
+++ LEVELS/LEVEL_1/mite_1.gd
@@ -3,28 +3,31 @@ extends Node2D
 @export var offset = Vector2(0., 16.)
 @export var duration = 6.0
 var tween: Tween
+@onready var animatedSprite = $mold_mite_1/AnimatedSprite2D
+@onready var collisionShape = $mold_mite_1/CollisionShape2D
+@onready var moldMite = $mold_mite_1
 
 func start():
-	$mold_mite_1/AnimatedSprite2D.play("walk_forward")
+	animatedSprite.play("walk_forward")
 	tween.play()
-	$mold_mite_1/CollisionShape2D.disabled = false
+	collisionShape.disabled = false
 	
 func stop():
-	$mold_mite_1/CollisionShape2D.disabled = true
+	collisionShape.disabled = true
 	tween.pause()
-	$mold_mite_1/AnimatedSprite2D.play("idle")
+	animatedSprite.play("idle")
 
 func _ready():
-	$mold_mite_1/AnimatedSprite2D.play("walk_forward")
+	animatedSprite.play("walk_forward")
 	start_tween()
 
 func start_tween():
 	tween = get_tree().create_tween()
 	tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	tween.set_loops().set_parallel(false)
-	tween.tween_property($mold_mite_1, "position", offset, duration / 2)
+	tween.tween_property(moldMite, "position", offset, duration / 2)
 	tween.tween_interval(2.0)
-	tween.tween_property($mold_mite_1, "position", Vector2.ZERO, duration / 2)
+	tween.tween_property(moldMite, "position", Vector2.ZERO, duration / 2)
 	tween.tween_interval(2.0)
 
 func _on_player_mite_1_collide() -> void:
