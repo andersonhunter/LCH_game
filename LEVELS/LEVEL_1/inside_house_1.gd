2693c2e72401a291872dbeadd1c4ad77b328c6f8
andersonhunter <andershu@oregonstate.edu>
2025-08-11 19:52:51 -0400

    solo bat only spawns when needed, ditto companion

--- LEVELS/LEVEL_1/inside_house_1.gd
+++ LEVELS/LEVEL_1/inside_house_1.gd
@@ -9,13 +9,17 @@ func transition() -> void:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if Global.has_bat:
+			$BatSolo.hide()
+			$BatSolo/CollisionShape2D.disabled = true
 	$bgm.volume_db = Global.music_volume
 	$bgm.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if $Player.get_real_velocity() != Vector2.ZERO && !$Bat.visible:
-		$Bat.show()
+	if Global.has_bat:
+		if $Player.get_real_velocity() != Vector2.ZERO && !$Bat.visible:
+			$Bat.show()
 
 func _on_leave_house_body_entered(body: Node2D) -> void:
 	if body == $Player:
