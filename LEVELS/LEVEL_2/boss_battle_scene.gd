6f010aa09af535379a9130f4a055b8ce5ce6d66c
andersonhunter <andershu@oregonstate.edu>
2025-08-12 00:36:31 -0400

    finished boss battle

---
+++ LEVELS/LEVEL_2/boss_battle_scene.gd
@@ -0,0 +1,224 @@
+extends Node2D
+
+var isDefending: bool = false
+var defends: int = 3
+signal battleDone
+
+var enemyStats = {
+	"boss": {
+		"base health": 15,
+		"current health": 15,
+		"attack": 3,
+		"defense": 1,
+		"speed": 4,
+	}
+}
+
+var player = preload("res://LEVELS/LEVEL_2/player_battle.tscn").instantiate()
+var sceneCharacters: Dictionary # {char: {stats}...}
+var turnQueue: Array # [char: Node2D, char: Node2D, ...]
+var enemies: Array
+var numEnemies: int
+
+func setLabelText(text: String) -> void:
+	$battleUI/textBox.show()
+	var tween: Tween = create_tween()
+	tween.tween_property($battleUI/textBox, "visible_ratio", 1.0, 1.5).from(0.0)
+	$battleUI/textBox.text = text
+	await tween.finished
+	return
+
+func setHealthBar(unit, health, maxHealth):
+	# Sets the unit's health bar
+	var healthBar: ProgressBar = unit.get_node("ProgressBar")
+	healthBar.max_value = maxHealth
+	healthBar.value = clamp(health, 0, maxHealth)
+	var healthFlavor: Label = unit.get_node("Label")
+	healthFlavor.text = "HP: %d/%d" % [healthBar.value, healthBar.max_value]
+
+func addEnemies():
+	# Adds enemies to scene depending on how many enemies spawn [1..3]
+	numEnemies = 1
+	# Remove superfluous enemies
+	match numEnemies:
+		1:
+			$enemies/enemy2.free()
+			$enemies/enemy3.free()
+		2:
+			$enemies/enemy1.free()
+	for parent in $enemies.get_children():
+		var enemy = load("res://LEVELS/LEVEL_2/boss.tscn").instantiate()
+		parent.add_child(enemy)
+		sceneCharacters[enemy] = enemyStats[enemy.name].duplicate()
+		turnQueue.push_back(enemy)
+		setHealthBar(
+			enemy, 
+			sceneCharacters[enemy]["current health"], 
+			sceneCharacters[enemy]["base health"]
+			)
+
+func sortTurnQueue():
+	# Sort the turn queue based on speed
+	turnQueue.sort_custom(
+		func(a, b): return sceneCharacters[a]["speed"] > sceneCharacters[b]["speed"]
+		)
+
+func characterDeath(character: Node2D) -> void:
+	match character.name:
+		"player":
+			print("Character died X.X")
+			return
+		_:
+			enemies.erase(character)
+			sceneCharacters.erase(character)
+			character.get_parent().free()
+			print(enemies.size())
+			if enemies.size() == 0:
+				# Switch scenes
+				Global.playerStats["exp"] += numEnemies
+				await get_tree().create_timer(0.25).timeout
+				# Remove scene from tree here
+				print("we're emitting")
+				battleDone.emit()
+			return
+
+func enemyTurn(enemy: Node2D):
+	$battleUI/commands.hide()
+	var damage = enemyStats[enemy.name]["attack"]
+	damage -= Global.playerStats["defense"]
+	if damage <= 0:
+		damage = 1
+	if isDefending:
+		damage = 0
+	var damageText = "%s pulses and deals %d damage" % [enemy.name, damage]
+	setLabelText(damageText)
+	await get_tree().create_timer(2.).timeout
+	Global.playerStats["current health"] -= damage
+	setHealthBar(
+		player, 
+		Global.playerStats["current health"], 
+		Global.playerStats["base health"])
+	$battleUI/textBox.text = ""
+	$battleUI/textBox.hide()
+	takeTurn()
+
+func playerTurn():
+	isDefending = false
+	$battleUI/commands/defend.disabled = false
+	for n in $battleUI/enemySelect.get_children():
+		n.disabled = true
+		n.hide()
+	if defends == 0:
+		$battleUI/commands/defend.disabled = true
+		$battleUI/commands/defend.hide()
+	$battleUI/commands.show()
+	$battleUI/commands/attack.grab_focus()
+
+func takeTurn():
+	$battleUI/commands.hide()
+	$battleUI/enemySelect.hide()
+	var turn = turnQueue.pop_front()
+	while not is_instance_valid(turn):  # Guard against invalid nodes
+		turn = turnQueue.pop_front()
+	turnQueue.push_back(turn)
+	if turn.name == player.name:
+		playerTurn()
+	else:
+		enemyTurn(turn)
+
+func attack(enemy: int) -> void:
+	var foe = $enemies.get_children()[enemy].get_children()[0]
+	var damage = (Global.playerStats["attack"] - sceneCharacters[foe]["defense"])
+	var damageText = "null deals %d damage to %s" % [damage, foe.name]
+	setLabelText(damageText)
+	await get_tree().create_timer(2.).timeout
+	sceneCharacters[foe]["current health"] -= damage
+	setHealthBar(foe, sceneCharacters[foe]["current health"], sceneCharacters[foe]["base health"])
+	if sceneCharacters[foe]["current health"] <= 0:
+		characterDeath(foe)
+	takeTurn()
+
+func _ready() -> void:
+	$battleUI/commands.hide()
+	get_node("player").add_child(player)
+	sceneCharacters[player] = Global.playerStats
+	turnQueue.push_back(player)
+	setHealthBar(player, Global.playerStats["current health"], Global.playerStats["base health"])
+	addEnemies()
+	sortTurnQueue()
+	$Camera2D.make_current()
+	takeTurn()
+
+func _on_attack_pressed() -> void:
+	$battleUI/commands.hide()
+	await setLabelText("Attack who?")
+	enemies = $enemies.get_children().map(
+		func(n): return n.get_children()[0]
+	)
+	$battleUI/enemySelect.show()
+	for n in range(enemies.size()):
+		$battleUI/enemySelect.get_children()[n].text = enemies[n].name
+		$battleUI/enemySelect.get_children()[n].show()
+		$battleUI/enemySelect.get_children()[n].disabled = false
+		if n == 0:
+			$battleUI/enemySelect/enemy1.grab_focus()
+
+func _on_defend_pressed() -> void:
+	$battleUI/commands.hide()
+	isDefending = true
+	defends -= 1
+	player.get_node("shields").get_children()[defends].hide()
+	match defends:
+		1:
+			setLabelText("%d defend remaining..." % [defends])
+		_:
+			setLabelText("%d defends remaining..." % [defends])
+	await get_tree().create_timer(2.).timeout
+	$battleUI/commands/defend.disabled = true
+	$battleUI/commands.show()
+	$battleUI/commands/attack.grab_focus()
+
+func _on_enemy_1_focus_entered() -> void:
+	if enemies.size() > 0:
+		enemies[0].get_node("targeted").show()
+		enemies[0].get_node("targeted").play()
+
+func _on_enemy_1_focus_exited() -> void:
+	if enemies.size() > 0:
+		enemies[0].get_node("targeted").hide()
+		enemies[0].get_node("targeted").stop()
+
+func _on_enemy_2_focus_entered() -> void:
+	if enemies.size() > 0:
+		enemies[1].get_node("targeted").show()
+		enemies[1].get_node("targeted").play()
+
+func _on_enemy_2_focus_exited() -> void:
+	if enemies.size() > 0:
+		enemies[1].get_node("targeted").hide()
+		enemies[1].get_node("targeted").stop()
+
+func _on_enemy_3_focus_entered() -> void:
+	if enemies.size() > 0:
+		enemies[2].get_node("targeted").show()
+		enemies[2].get_node("targeted").play()
+
+func _on_enemy_3_focus_exited() -> void:
+	if enemies.size() > 0:
+		enemies[2].get_node("targeted").hide()
+		enemies[2].get_node("targeted").stop()
+
+func _on_enemy_1_pressed() -> void:
+	$battleUI/textBox.text = ""
+	$battleUI/textBox.hide()
+	attack(0)
+
+func _on_enemy_2_pressed() -> void:
+	$battleUI/textBox.text = ""
+	$battleUI/textBox.hide()
+	attack(1)
+
+func _on_enemy_3_pressed() -> void:
+	$battleUI/textBox.text = ""
+	$battleUI/textBox.hide()
+	attack(2)
--- LEVELS/LEVEL_2/silicon_swamp.gd
+++ LEVELS/LEVEL_2/silicon_swamp.gd
@@ -1,7 +1,33 @@
 extends Node
 var battleScene = preload("res://LEVELS/LEVEL_2/battle_scene.tscn").instantiate()
+var bossBattleScene = preload("res://LEVELS/LEVEL_2/boss_battle_scene.tscn").instantiate()
 var removeEnemy = null
 var cosmicBrownie = preload("res://LEVELS/LEVEL_2/cosmic_brownie.tscn").instantiate()
+var bossBattle: bool = false
+@onready var player = $overworld/Player
+signal startDialog(message)
+
+@onready var sceneDialog = [
+	[
+		"[color=green]A powerful light washes over you...[/color]",
+		"[color=green]You tremble at its power.[/color]",
+		"[color=green]You fear you must grow stronger...[/color]"
+	], 
+	[
+		"[color=green]A powerful light washes over you...[/color]",
+		"[color=green]The strength within your heart [/color]",
+		"[color=green]pulses even stronger.[/color]",
+		"[color=green][wave]BRACE YOURSELF[/wave][/color]"
+	],
+	[
+		"[color=green]You feel the light before you [/color]",
+		"[color=green]dissipate, returning to the world. [/color]",
+		"[color=green]Still, you feel a sense of unease...[/color]",
+		"[color=green]You will have to check back... [/color]",
+		"[color=green]... when the dev is done![/color]",
+		"[color=green]Thank you for playing <3 <3[/color]"
+	]
+]
 
 func startBattle():
 	removeEnemy = $overworld/Player.get_last_slide_collision().get_collider()
@@ -12,7 +38,34 @@ func startBattle():
 	battleScene.battleDone.connect(_on_battleDone)
 
 func startBossBattle():
-	pass
+	if Global.has_bat:
+		$overworld.get_node("bat").hide()
+	var dialog = player.get_node("Dialogue")
+	if Global.playerStats["level"] < 2:
+		player.speed = 0
+		player.position += Vector2(0., 16.)
+		dialog.show()
+		startDialog.emit(sceneDialog[0])
+		await dialog.textCompleted
+		dialog.hide()
+		$overworld/boss/CollisionShape2D.disabled = true
+		if Global.has_bat:
+			$overworld.get_node("bat").show()
+		player.speed = Global.speed
+		$overworld/boss/CollisionShape2D.disabled = false
+	else:
+		removeEnemy = $overworld/Player.get_last_slide_collision().get_collider()
+		player.get_node("CollisionShape2D").disabled = true
+		bossBattle = true
+		player.speed = 0
+		player.position += Vector2(0., 16.)
+		dialog.show()
+		startDialog.emit(sceneDialog[1])
+		await dialog.textCompleted
+		dialog.hide()
+		$overworld.propagate_call("hide")
+		self.add_child(bossBattleScene)
+		bossBattleScene.battleDone.connect(_on_battleDone)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -37,21 +90,39 @@ func _process(delta: float) -> void:
 				)
 
 func _on_battleDone():
-	self.remove_child(self.get_node("battleScene"))
-	self.add_child(cosmicBrownie)
-	cosmicBrownie.position = removeEnemy.position
-	cosmicBrownie.get_node("AnimatedSprite2D").play("default")
-	removeEnemy.queue_free()
-	await removeEnemy.tree_exited
-	$overworld/Player/CollisionShape2D.disabled = false
-	$overworld.propagate_call("show")
-	$overworld/Player/enterPrompt.hide()
-	$overworld/Player/Dialogue.hide()
-	$overworld/Player/enterNewAreaPrompt.hide()
-	$overworld/Player/levelUp.hide()
-	if Global.levelUp():
-		$overworld/Player/levelUp.show()
-		await get_tree().create_timer(1.0).timeout
+	if bossBattle:
+		print(get_node("bossBattleScene"))
+		self.remove_child(self.get_node("bossBattleScene"))
+		removeEnemy.queue_free()
+		await removeEnemy.tree_exited
+		$overworld.propagate_call("show")
+		$overworld/Player/enterPrompt.hide()
+		$overworld/Player/Dialogue.hide()
+		$overworld/Player/enterNewAreaPrompt.hide()
 		$overworld/Player/levelUp.hide()
-	battleScene = preload("res://LEVELS/LEVEL_2/battle_scene.tscn").instantiate()
-	cosmicBrownie = preload("res://LEVELS/LEVEL_2/cosmic_brownie.tscn").instantiate()
+		var dialog = player.get_node("Dialogue")
+		Global.isDark = false
+		dialog.show()
+		startDialog.emit(sceneDialog[2])
+		await dialog.textCompleted
+		dialog.hide()
+		get_tree().quit()
+	else:
+		self.remove_child(self.get_node("battleScene"))
+		self.add_child(cosmicBrownie)
+		cosmicBrownie.position = removeEnemy.position
+		cosmicBrownie.get_node("AnimatedSprite2D").play("default")
+		removeEnemy.queue_free()
+		await removeEnemy.tree_exited
+		$overworld/Player/CollisionShape2D.disabled = false
+		$overworld.propagate_call("show")
+		$overworld/Player/enterPrompt.hide()
+		$overworld/Player/Dialogue.hide()
+		$overworld/Player/enterNewAreaPrompt.hide()
+		$overworld/Player/levelUp.hide()
+		if Global.levelUp():
+			$overworld/Player/levelUp.show()
+			await get_tree().create_timer(1.0).timeout
+			$overworld/Player/levelUp.hide()
+		battleScene = preload("res://LEVELS/LEVEL_2/battle_scene.tscn").instantiate()
+		cosmicBrownie = preload("res://LEVELS/LEVEL_2/cosmic_brownie.tscn").instantiate()
